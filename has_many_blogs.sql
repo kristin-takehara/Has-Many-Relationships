21f919aa30e8a74cce3decd250294245c8502733
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-14 14:45:37 -1000

    ssl/firewall incorrect row count

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,19 +1,19 @@
 -- ### SQL 1-n Relationships and Joins Exercise in PostgreSQL
 -- ## Setup schema and relationships
--- \c KRISTINTAKEHARA
--- DROP DATABASE has_many_blogs;
--- DROP USER has_many_user;
+\c KRISTINTAKEHARA;
+DROP DATABASE has_many_blogs;
+DROP USER has_many_user;
 
--- -- 1. Create a new postgres user named `has_many_user`
+-- -- -- 1. Create a new postgres user named `has_many_user`
 
 CREATE USER has_many_user;
 
--- -- 1. Create a new database named `has_many_blogs` owned by `has_many_user`
+-- 1. Create a new database named `has_many_blogs` owned by `has_many_user`
 
--- CREATE DATABASE has_many_blogs
--- OWNER has_many_user;
+CREATE DATABASE has_many_blogs
+OWNER has_many_user;
 
---  \c has_many_blogs;
+ \c has_many_blogs;
 -- 1. Before each create table statement, add a drop table if exists statement.
 -- 1. In `has_many_blogs.sql` Create the tables (including any PKs, Indexes, and Constraints that you may need) to fulfill the requirements of the **has_many_blogs schema** below.
 
@@ -25,8 +25,8 @@ id            serial        not null    PRIMARY KEY,
 username      varchar(90)   null        default null,
 first_name    varchar(90)   null        default null,
 last_name     varchar(90)   null        default null,
-created_at    timestamptz   DEFAULT     now(),
-updated_at    timestamptz   DEFAULT     now()
+created_at    timestamptz   not null    DEFAULT now(),
+updated_at    timestamptz   not null    DEFAULT now()
 );
 
 -- DROP TABLE IF EXISTS posts;
@@ -35,19 +35,19 @@ id            serial        not null    PRIMARY KEY,
 title         varchar(180)  null        default null,
 url           varchar(510)  null        default null,
 content       text          null        default null,
-created_at    timestamptz   default     now(),
-updated_at    timestamptz   default     now(),
-user_id       integer       references  users(id)
+created_at    timestamptz   not null    DEFAULT now(),
+updated_at    timestamptz   not null    DEFAULT now(),
+user_id       serial        references  users(id)
 );
 
 -- DROP TABLE IF EXISTS comments;
 CREATE TABLE comments (
 id            serial        not null    PRIMARY KEY,
 body          varchar(510)  null        DEFAULT null,
-created_at    timestamptz   DEFAULT     now(),
-updated_at    timestamptz   DEFAULT     now(),
-post_id       integer       references  posts(id),
-user_id       integer       references  users(id)
+created_at    timestamptz   not null    DEFAULT now(),
+updated_at    timestamptz   not null    DEFAULT now(),
+post_id       serial        references  posts(id),
+user_id       serial        references  users(id)
 );
 
 -- -- -- 1. Create the necessary FKs needed to relate the tables according to the **relationship table** below.
---
+++ joins.sql
@@ -0,0 +1,104 @@
+-- Queries across multiple tables
+-- Write the following SQL statements in joins.sql
+-- \c has_many_blogs;
+-- Create a query to get all fields from the users table
+
+-- SELECT id, username, first_name, last_name, created_at, updated_at
+-- FROM users;
+
+-- -- Create a query to get all fields from the posts table where the user_id is 100
+
+-- SELECT id, title, url, content, created_at, updated_at, user_id
+-- FROM posts
+-- WHERE user_id = 100;
+
+-- -- Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
+
+-- SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, p.user_id, u.id, u.first_name, u.last_name
+-- FROM posts p
+-- JOIN users u ON p.user_id = u.id
+-- WHERE p.user_id = 200;
+
+-- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
+
+-- SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, p.user_id, u.id, u.username, u.first_name, u.last_name
+-- FROM posts p
+-- JOIN users u ON p.user_id = u.id;
+-- WHERE u.first_name = 'Norene'
+-- AND u.last_name = 'Schmitt';
+
+-- Create a query to get usernames from the users table where the user has created a post after January 1, 2015
+
+-- SELECT u.username
+-- FROM users u
+-- JOIN posts p ON p.user_id = u.id
+-- WHERE p.created_at > '2015-01-01'
+-- ORDER BY p.created_at;
+
+-- -- Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
+
+-- SELECT p.title, p.content, u.username
+-- FROM posts p
+-- JOIN users u ON p.user_id = u.id
+-- WHERE u.created_at > '2015-01-01';
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
+
+-- SELECT c.id, c.body, c.created_at, c.updated_at, p.title AS "Post Title"
+-- FROM comments c
+-- JOIN posts p ON c.post_id = p.id;
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
+
+-- SELECT c.id, c.body AS "comment_body", c.created_at, c.updated_at, p.title AS "post_title", p.url AS "post_url", p.created_at
+-- FROM comments c
+-- JOIN posts p ON c.post_id = p.id
+-- WHERE p.created_at < '2015-01-01';
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created after January 1, 2015
+
+-- SELECT c.id, c.body AS "comment_body", c.created_at, c.updated_at, p.title AS "post_title", p.url AS "post_url", p.created_at
+-- FROM comments c
+-- JOIN posts p ON c.post_id = p.id
+-- WHERE p.created_at > '2015-01-01';
+
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the comment body contains the word 'USB'
+
+-- SELECT c.id, c.body AS "comment_body", c.created_at, c.updated_at, p.title AS "post_title", p.url AS "post_url"
+-- FROM comments c
+-- JOIN posts p ON p.id = c.post_id
+-- WHERE c.body LIKE '%USB%';
+
+-- Create a query to get the post title (aliased as post_title), first name of the author of the post, last name of the author of the post, and comment body (aliased to comment_body), where the comment body contains the word 'matrix' ( should have 855 results )
+
+-- SELECT p.title AS "post_title", u.first_name, u.last_name, c.body AS "comment_body"
+-- FROM comments c
+-- JOIN posts p ON p.id = c.post_id
+-- JOIN users u ON u.id = c.user_id
+-- WHERE c.body LIKE '%matrix%';
+
+-- Create a query to get the first name of the author of the comment, last name of the author of the comment, and comment body (aliased to comment_body), where the comment body contains the word 'SSL' and the post content contains the word 'dolorum' ( should have 102 results )
+
+-- SELECT u.first_name, u.last_name, c.body AS "comment_body"
+-- FROM comments c
+-- JOIN posts p ON p.id = c.post_id
+-- JOIN users u ON u.id = c.user_id
+-- WHERE c.body LIKE '%SSL%'
+-- AND p.content LIKE '%dolorum%';
+
+-- Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
+
+SELECT p.title AS "post_title", c.body AS "comment_body", u.first_name AS "post_author_first_name", u.last_name AS "post_author_last_name",  u.username AS "comment_author_username"
+FROM comments c
+JOIN posts p ON p.id = c.post_id
+JOIN users u ON u.id = c.user_id
+-- WHERE (c.body LIKE '%SSL%' OR c.body LIKE '%firewall%')
+WHERE p.content LIKE '%nemo%';
+
+
+-- Additional Queries
+
+-- If you finish early, perform and record the following SQL statements in joins.sql using these higher level requirements.
+
+-- Count how many comments have been written on posts that have been created after July 14, 2015 ( should have one result, the value of the count should be 27)
+-- Find all users who comment about 'programming' ( should have 336 results)
